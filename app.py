b0404b3bbffe58f4fbc362ce37a831c0bc6fef53
Rakshan Hegde <69235121+var787@users.noreply.github.com>
2021-11-13 16:36:55 -0600

    completed

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 # Guess the number
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+          guess = int(input(f'Guess a number between 1 & {x}:'))
+          if  random_number > guess:
+            print('Sorry, too low')
+          elif random_number < guess:
+            print('Sorry, too high')
+    print(f'Yay!!, you guessed the right number {random_number}')
+
+
+
+guess(10)
